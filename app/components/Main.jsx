9672f6547bab9f6e112b5434049420ed65bbb12d
Fiona <finara.k@hotmail.com>
2018-01-03 00:46:30 +0100

    fix up router

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -3,14 +3,14 @@ var Navigation = require('Navigation');
 
 var Main = (props) => {
     return (
-      <div>
-        <Navigation/>
-        <div className="grid-x">
-          <div className="medium-6 large-4 cell small-centered">
-            {props.children}
-          </div>
+        <div>
+            <Navigation/>
+            <div className="grid-x">
+                <div className="medium-6 large-4 cell small-centered">
+                    {props.children}
+                </div>
+            </div>
         </div>
-      </div>
     );
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -23,8 +23,10 @@ module.exports = {
     root: __dirname,
     alias: {
       applicationStyles: 'app/styles/app.scss',
+      Countdown: 'app/components/Countdown.jsx',
       Main: 'app/components/Main.jsx',
-      Navigation: 'app/components/Navigation.jsx'
+      Navigation: 'app/components/Navigation.jsx',
+      Timer: 'app/components/Timer.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
