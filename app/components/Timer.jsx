34960096ce7662931fb55ebc2a26731ba7bff36c
Fiona <finara.k@hotmail.com>
2018-01-03 18:25:09 +0100

    fix up timer

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,11 +1,60 @@
 var React = require('react');
 var Clock = require('Clock');
+var Controls = require('Controls');
 
 var Timer = React.createClass({
-    render: function (){
-        return <div>
-            <Clock totalSeconds={568}/>
-        </div>
+    getInitialState: function () {
+        return {
+            count: 0,
+            timerStatus: 'stopped'
+        };
+    },
+    componentDidUpdate: function (prevProps, prevState) {
+        if (this.state.timerStatus !== prevState.timerStatus) {
+            switch (this.state.timerStatus) {
+                case 'started':
+                    this.startTimer();
+                    break;
+                case 'stopped':
+                    this.setState({count: 0});
+                case 'paused':
+                    clearInterval(this.timer)
+                    this.timer = undefined;
+                    break;
+            }
+        }
+    },
+    componentWillUnmount: function () {
+        clearInterval(this.timer);
+        this.timer = undefined;
+    },
+    startTimer: function () {
+        this.timer = setInterval(() => {
+            var newCount = this.state.count + 1;
+            this.setState({
+               count: newCount
+            });
+            if (newCount === 10000) {
+                this.setState({timerStatus: 'stopped'});
+            }
+        }, 1000);
+    },
+    handleStatusChange: function (newStatus) {
+        this.setState({
+            timerStatus: newStatus
+        });
+    },
+    render: function () {
+        var {count, timerStatus} = this.state;
+
+        return (
+            <div>
+                <h1 className="page-title">Timer App</h1>
+                <p className="page-sub-title">up to 10'000 seconds</p>
+                <Clock totalSeconds={count}/>
+                <Controls countdownStatus={timerStatus} onStatusChange={this.handleStatusChange}/>
+            </div>
+        );
     }
 });
 
